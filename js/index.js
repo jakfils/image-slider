131f75ae3e57d6fc8aee62e088e27d449fc52290
jakfils <jakfilstechno@gmail.com>
2023-07-19 12:14:34 +0100

    Add functionnalities

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const btns = document.querySelector(".buttons");
 const prev = document.querySelector(".previous");
 const next = document.querySelector(".next");
 const image = document.querySelector(".image");
+const images = document.querySelector(".images");
 console.log(img);
 console.log(btns);
 
@@ -11,6 +12,8 @@ console.log(btns);
 let id = 1;
 // Number of images
 const numberOfImgs = 7;
+
+let position = img.width+10;
 
 // Function handle Buttons
 const handleBtns = () => {
@@ -25,36 +28,41 @@ const handleBtns = () => {
 };
 
 // Fucntion Show Image
-const showImage = (id, direction) => {
-  img.setAttribute("src", "./img/img0" + id + ".jpg");
-};
+// const showImage = (id, direction) => {
+//   img.setAttribute("src", "./img/img0" + id + ".jpg");
+// };
 
-function reset_animation() {
-  image.style.animation = "none";
-  image.offsetHeight; /* trigger reflow */
-  image.style.animation = null;
-}
+// function reset_animation() {
+//   image.style.animation = "none";
+//   image.offsetHeight; /* trigger reflow */
+//   image.style.animation = null;
+// }
 
 handleBtns();
 console.log("Initial Id = " + id);
 
 // Buttons event Click listener
+console.log(img.width);
+
 
 btns.addEventListener("click", (e) => {
   if (e.target.textContent == "Next") {
-    reset_animation();
-    image.style.animation = "leftToRight 1s";
+    // reset_animation();
+    images.style.transform = `translateX(-${position}px)`;
+    position+=position;
+    console.log(position);
+    
+
     id++;
     handleBtns();
-    img.setAttribute("src", "./img/img0" + id + ".jpg");
-    // image.style.transform = "translate(100)";
+
     console.log("Next btn");
     console.log("Next Id= " + id);
   } else if (e.target.textContent == "Prev") {
-    reset_animation();
+    // reset_animation();
     id--;
     img.setAttribute("src", "./img/img0" + id + ".jpg");
-    image.style.animation = "rightToLeft 1s";
+    // image.style.animation = "rightToLeft 1s";
     handleBtns();
     console.log("Prev button");
     console.log("Prev Id =" + id);
